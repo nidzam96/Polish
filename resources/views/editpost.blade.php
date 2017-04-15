ae9a50369a0f9fb8240c01df6b8d12e20adf0766
Nidzam <nidzamhussain96@gmail.com>
2017-04-15 16:27:29 +0800

    changing editpost.blade.php

---
+++ resources/views/editpost.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-8"><h1>Edit Post</h1></div>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    Call Me xxx-xxxxxxx
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
